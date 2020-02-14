ff813b022f291a420c327b1f9405b2efa6c9e5ab
crezqf_hi@126.com <crezqf_hi@126.com>
2020-02-14 13:34:46 +0800

    add maven package plugins

--- service-gateway/src/main/java/org/creation/gateway/GatewayApplication.java
+++ service-gateway/src/main/java/org/creation/gateway/GatewayApplication.java
@@ -1,7 +1,6 @@
 package org.creation.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
